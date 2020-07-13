f1de3d985569c9e553ab2fd4aacf2c034120fd14
Gregory <gregory09ruiz@gmail.com>
2020-07-13 17:22:52 -0400

    roles_migration:ready

--- database/migrations/2020_07_13_211232_create_roles_table.php
+++ database/migrations/2020_07_13_211232_create_roles_table.php
@@ -15,6 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 128);
             $table->timestamps();
         });
     }
