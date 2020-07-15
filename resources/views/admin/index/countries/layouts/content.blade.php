0161c9d365f5f2887f7c166baf77488fdfc142cc
Gregory <gregory09ruiz@gmail.com>
2020-07-15 16:21:59 -0400

    content:blade.councils

--- resources/views/admin/index/countries/layouts/content.blade.php
+++ resources/views/admin/index/countries/layouts/content.blade.php
@@ -87,7 +87,7 @@
               <div class="card">
                 <!-- Card header -->
                 <div class="card-header border-0">
-                  <h3 class="mb-0">Paises Registradas</h3>
+                  <h3 class="mb-0">Paises Registrados</h3>
                 </div>
                 <!-- Light table -->
                 <div class="table-responsive">
