afa918a850e2779bb8adbd8c252476fe3f437f9c
Gregory <gregory09ruiz@gmail.com>
2020-07-15 13:31:09 -0400

    fonts

---
+++ resources/views/admin/index/layouts/navbar.blade.php
@@ -0,0 +1,53 @@
+<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+      <!-- Brand -->
+      <div class="sidenav-header  align-items-center">
+        <a class="navbar-brand" href="javascript:void(0)">
+          <img src="{{ asset('admin/img/blue.png') }}" class="navbar-brand-img" alt="Logo de la Aplicacion">
+        </a>
+      </div>
+      <div class="navbar-inner">
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Panel de Control</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="vacantes_gerente.html">
+                <i class="ni ni-bullet-list-67 text-default"></i>
+                <span class="nav-link-text">Vacantes</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="reportes_gerente.html">
+                <i class="fa fa-chart-area text-info"></i>
+                <span class="nav-link-text">Reportes</span>
+              </a>
+            </li>
+
+          </ul>
+          <!-- Divider -->
+          <!-- Heading -->
+        <h6 class="navbar-heading p-0 text-muted">
+          </h6>
+          <!-- Navigation -->
+          <hr class="my-3">
+
+          <ul class="navbar-nav mb-md-3">
+
+            <li class="nav-item">
+              <a class="nav-link active active-pro" href="examples/upgrade.html">
+                <i class="ni ni-send text-dark"></i>
+                <span class="nav-link-text">Soporte Técnico</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
