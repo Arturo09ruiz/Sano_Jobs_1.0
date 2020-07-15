31d90dea4e6153a2bb6fe995c55e3275912e4045
Gregory <gregory09ruiz@gmail.com>
2020-07-15 13:09:57 -0400

    boostrap_js

--- resources/views/admin/index/layouts/index.blade.php
+++ resources/views/admin/index/layouts/index.blade.php
@@ -33,7 +33,7 @@
 
 
     <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('admin/vendor/bootstrap_js/bootstrap.bundle.min.js') }}"></script>
     <script src="assets/vendor/js-cookie/js.cookie.js"></script>
     <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
     <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
