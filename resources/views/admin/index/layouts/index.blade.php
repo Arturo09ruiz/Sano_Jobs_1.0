067b3196a358ce3b49011b2f9a6b96eda1a758c5
Gregory <gregory09ruiz@gmail.com>
2020-07-15 12:57:50 -0400

    css_font_index

---
+++ resources/views/admin/index/layouts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Plantilla Sano Jobs Para el Departamento de Autosuficiencia y Bienestar">
+  <meta name="author" content="Arturo Ruiz">
+  <title>Sano Jobs - Panel Administrativo</title>
+  <!-- Favicon -->
+  <!-- <link rel="icon" href="assets/img/brand/favicon.png" type="image/png"> -->
+  <!-- Fonts -->
+  <link rel="stylesheet" href="{{ asset('admin/fonts/1.css') }}">
+  <!-- Icons -->
+  <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <!-- Page plugins -->
+  <!-- Argon CSS -->
+  <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
+</head>
+
+<body>
