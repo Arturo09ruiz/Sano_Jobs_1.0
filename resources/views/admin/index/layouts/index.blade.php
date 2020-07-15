fe9a9e5d584bd52015b64ebe0256cb0604bf90b1
Gregory <gregory09ruiz@gmail.com>
2020-07-15 13:00:46 -0400

    nucleo_icons

--- resources/views/admin/index/layouts/index.blade.php
+++ resources/views/admin/index/layouts/index.blade.php
@@ -11,8 +11,8 @@
   <!-- Fonts -->
   <link rel="stylesheet" href="{{ asset('admin/fonts/1.css') }}">
   <!-- Icons -->
-  <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
-  <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <link rel="stylesheet" href="{{ asset('admin/vendor/nucleo/nucleo.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ asset('admin/vendor/font-awesome/') }}" type="text/css">
   <!-- Page plugins -->
   <!-- Argon CSS -->
   <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
