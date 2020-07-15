a17f163f7159a14001bdb8a21e097431edf0f955
Gregory <gregory09ruiz@gmail.com>
2020-07-15 17:13:36 -0400

    cities_link-admin-panel

--- resources/views/admin/index/layouts/navbar.blade.php
+++ resources/views/admin/index/layouts/navbar.blade.php
@@ -37,7 +37,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="reportes_gerente.html">
+                <a class="nav-link" href="{{route('cities.index')}}">
                   <i class="fa fa-city "></i>
                   <span class="nav-link-text">Ciudades</span>
                 </a>
