ca35ecacddf02908ae512eb1cefb4c319893ef31
Gregory <gregory09ruiz@gmail.com>
2020-07-15 16:09:51 -0400

    Council_model_ready

--- app/Council.php
+++ app/Council.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Council extends Model
 {
-    //
+
+    protected $fillable =[
+        'name','country_id'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo('App\Country', 'country_id', 'id');
+    }
 }
