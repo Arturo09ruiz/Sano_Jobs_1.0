2f8b1cee311a437065bf1219686e5695004ac0e2
Gregory <gregory09ruiz@gmail.com>
2020-07-15 11:25:15 -0400

    css

--- resources/views/web/index/app.blade.php
+++ resources/views/web/index/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,5 +15,13 @@
   <!-- Font Awesome Icons -->
   <link href="{{ asset('vendor/font-awesome/font-awesome.min.css') }}" rel="stylesheet" />
   <!-- CSS Files -->
-  <link href="assets/css/argon-design-system.css?v=1.2.0" rel="stylesheet" />
+  <link href="{{ asset('css/1.css') }}" rel="stylesheet" />
 </head>
+
+
+<body class="landing-page">
+    @yield('navbar-index')
+
+
+</body>
+</html>
