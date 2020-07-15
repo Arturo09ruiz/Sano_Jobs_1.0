8744db76ed8239e7b5943171f5ebfde81ffa6dcb
Gregory <gregory09ruiz@gmail.com>
2020-07-15 16:16:24 -0400

    page_blade.councils

---
+++ resources/views/admin/index/councils/layouts/page.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Plantilla Sano Jobs Para el Departamento de Autosuficiencia y Bienestar">
+  <meta name="author" content="Arturo Ruiz">
+  <title>Sano Jobs - Panel Administrativo</title>
+  <!-- Favicon -->
+  <!-- <link rel="icon" href="assets/img/brand/favicon.png" type="image/png"> -->
+  <!-- Fonts -->
+  <link rel="stylesheet" href="{{ asset('admin/fonts/1.css') }}">
+  <!-- Icons -->
+  <link rel="stylesheet" href="{{ asset('admin/vendor/nucleo/nucleo.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ asset('admin/vendor/font-awesome/all.min.css') }}" type="text/css">
+  <!-- Page plugins -->
+  <!-- CSS -->
+  <link rel="stylesheet" href="{{ asset('admin/css/1.css') }}" type="text/css">
+</head>
+
+<body>
+    @include('admin.index.layouts.navbar')
+    @include('admin.index.councils.layouts.content')
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap_js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/js-cookie/js.cookie.js') }}"></script>
+    <script src="{{ asset('admin/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/jquery-scroll-lock/jquery-scrollLock.min.js') }}"></script>
+    <!-- Optional JS -->
+    <script src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/chart.js/Chart.extension.js') }}"></script>
+    <!-- Arturo JS -->
+    <script src="{{ asset('admin/js/1.js') }}"></script>
+
+</body>
+</html>
