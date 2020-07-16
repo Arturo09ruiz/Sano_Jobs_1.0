ebef46960798c190cd0e5b4816f97a7fc2ebba3c
Arturo <gregory09ruiz@gmail.com>
2020-07-16 14:03:21 -0400

    rol_form

--- resources/views/admin/index/roles/layouts/create.blade.php
+++ resources/views/admin/index/roles/layouts/create.blade.php
@@ -1,21 +1,21 @@
 
-  <div class="modal fade" id="registrar_pais" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="registrar_rol" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Registrar Pais</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Registrar Rol</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <form action="{{route('countries.store')}}" method="POST">
+        <form action="{{route('roles.store')}}" method="POST">
 
         <div class="modal-body">
                 @csrf
                 <div class="form-group">
                   <div class="input-group input-group-alternative mb-3">
                     <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fas fa-globe-americas"></i></span>
+                      <span class="input-group-text"><i class="fa fa-user-tag"></i></span>
                     </div>
                     <input class="form-control" placeholder="Ingresar Nombre" name="name" required type="text">
                   </div>
@@ -23,7 +23,7 @@
         </div>
             <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-            <button  type="submit" class="btn test-buuton btn-primary">Registrar Pais</button>
+            <button  type="submit" class="btn test-buuton btn-primary">Registrar Rol</button>
         </form>
 
         </div>
