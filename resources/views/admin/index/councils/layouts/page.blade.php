80e779d8bd3e377a0dba8719d663768a1c513efb
Arturo <gregory09ruiz@gmail.com>
2020-07-16 20:56:04 -0400

    councils datatable

--- resources/views/admin/index/councils/layouts/page.blade.php
+++ resources/views/admin/index/councils/layouts/page.blade.php
@@ -54,5 +54,39 @@
           $('.js-example-basic-single').select2();
       });
       </script>
+        <script src="{{ asset('admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+        <script>
+        $(document).ready(function() {
+          $('#councils').DataTable({
+              "language": {
+
+                  "sProcessing":     "Procesando...",
+                  "sLengthMenu":     "Mostrar _MENU_ registros",
+                  "sZeroRecords":    "No se encontraron resultados",
+                  "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                  "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+                  "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0",
+                  "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                  "sInfoPostFix":    "",
+                  "sSearch":         "Buscar:",
+                  "sUrl":            "",
+                  "sInfoThousands":  ",",
+                  "sLoadingRecords": "Cargando...",
+                  "oPaginate": {
+                  "sFirst":    "Primero",
+                  "sLast":     "Último",
+                  "sNext":     ">",
+                  "sPrevious": "<"
+                  },
+                  "oAria": {
+                      "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                      "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                  }
+
+      }
+          });
+          });
+        </script>
 </body>
 </html>
