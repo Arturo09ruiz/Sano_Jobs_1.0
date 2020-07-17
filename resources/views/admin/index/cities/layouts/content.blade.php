ae25c66a49f0056d40ef955f63f67b8e6e4ef571
Arturo <gregory09ruiz@gmail.com>
2020-07-17 11:09:24 -0400

    cities

--- resources/views/admin/index/cities/layouts/content.blade.php
+++ resources/views/admin/index/cities/layouts/content.blade.php
@@ -87,7 +87,7 @@
               <div class="card">
                 <!-- Card header -->
                 <div class="card-header border-0">
-                  <h3 class="mb-0">Ciudades Registrados</h3>
+                  <h3 class="mb-0">Ciudades Registradas</h3>
                 </div>
                 <!-- Light table -->
                 <div class="table-responsive">
@@ -147,7 +147,7 @@
                     </tbody>
                   </table>
                 </div>
-               
+
 
               </div>
             </div>
