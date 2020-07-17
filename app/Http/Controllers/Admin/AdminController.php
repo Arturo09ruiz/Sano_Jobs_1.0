80d5c49b29d61c82efeffb518fe0cab0b7f8456b
Arturo <gregory09ruiz@gmail.com>
2020-07-17 15:57:47 -0400

    reditect_if_auth

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
+
+
     public function index()
     {
-        // imprimir maravilla
-        // dd($tags);
+        $role_id = Auth::user()->role_id;
+        if($role_id==1){
+
+        }else{
+            return redirect()->route('index');
+        }
+
+
+
+
         return view('admin.index.app');
     }
 }
