3b1f79bb6856119b8d4525c7d01667d6ff86d349
Arturo <gregory09ruiz@gmail.com>
2020-07-22 17:13:03 -0400

    create_blade.vacant_name

--- resources/views/admin/index/vacants/layouts/create.blade.php
+++ resources/views/admin/index/vacants/layouts/create.blade.php
@@ -1,9 +1,9 @@
 
-  <div class="modal fade" id="registrar_team" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="registrar_vacant" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Registrar Equipo</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Registrar Vacante</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
