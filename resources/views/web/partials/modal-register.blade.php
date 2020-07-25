ec1bc499bcefd2ee47141e9952a97e568b56b4c6
Arturo <gregory09ruiz@gmail.com>
2020-07-25 11:47:05 -0400

    modal_in:the view

--- resources/views/web/partials/modal-register.blade.php
+++ resources/views/web/partials/modal-register.blade.php
@@ -1,6 +1,6 @@
 
 <!-- REGISTER MODAL -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
+<div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
